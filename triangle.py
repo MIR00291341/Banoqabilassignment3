0b402bac199deaf1e96e55a6bbd29c8ed5201e14
Muhammad Ismail Rasheed <159624882+MIR00291341@users.noreply.github.com>
2024-07-07 14:46:15 +0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -3,10 +3,6 @@ b = float(input("Enter the second side: "))
 c = float(input("Enter the third side: "))
 
 
-if a + b <= c or a + c <= b or b + c <= a:
-    print("Invalid triangle")
-else:
-
     if a == b == c:
         print("Equilateral triangle")
     elif a == b or a == c or b == c:
